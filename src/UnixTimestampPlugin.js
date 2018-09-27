ccf412a8299a1594a9f4dddf217be9ea799ab0b3
Julian Müller <j.mueller@s-v.de>
2018-09-27 14:18:24 +0200

    ⚡️ Hello world!

---
+++ src/UnixTimestampPlugin.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+class UnixTimestampPlugin {
+    constructor(options) {
+        const defaultOptions = {
+            filePath: './',
+            fileName: '.build-timestamp',
+        };
+
+        this.options = Object.assign({}, defaultOptions, options);
+    }
+
+    apply(compiler) {
+        compiler.plugin('done', function(compilation, callback, options=this.options) {
+            console.log(`Writing build timestamp... ${compilation.endTime}`);
+
+            fs.writeFileSync(
+                `${options.filePath}/${options.fileName}`,
+                compilation.endTime,
+                'utf8'
+            )
+        }.bind(this));
+    }
+}
+
+export default UnixTimestampPlugin;
